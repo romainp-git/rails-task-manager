6013fa23cde8fe0a5c3e6b82b661b7c836fbf18c
romain portier <romain.portier@wanadoo.fr>
2024-11-12 14:00:51 +0100

    Tasks Manager

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,19 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get    "tasks",           to: "tasks#index"
+
+  # CREATE
+  get    "tasks/new",       to: "tasks#new",      as: :new_task
+  post   "tasks",           to: "tasks#create"
+
+  # READ
+  get    "tasks/:id",       to: "tasks#show",     as: :task
+
+  # UPDATE
+  get    "tasks/:id/edit",  to: "tasks#edit",     as: :edit_task
+  patch  "tasks/:id",       to: "tasks#update"
+
+  # DELETE
+  delete "tasks/:id",       to: "tasks#destroy"
 end
---
+++ db/migrate/20241112100059_add_false_default_value_to_completed_in_task.rb
@@ -0,0 +1,5 @@
+class AddFalseDefaultValueToCompletedInTask < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default(:tasks, :completed, false)
+  end
+end
